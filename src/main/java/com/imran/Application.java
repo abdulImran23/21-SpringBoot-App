e3d6fcf6816ac8622ba574d5b7e8f9615f7c4695
unknown <abdul23imran@gmail.com>
2020-12-05 13:37:45 +0530

    added int variable

--- src/main/java/com/imran/Application.java
+++ src/main/java/com/imran/Application.java
@@ -10,6 +10,7 @@ public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
 		//changes for HIS-134
+                    int a=10;
 		run.close();
 	}
 
